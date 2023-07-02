d5018959a516157965d74c2d49cddb4e7d2c7b8c
bamjam101 <herejamil123@gmail.com>
2023-07-02 17:42:57 +0530

    Created a modal inteface for creating new community.

--- src/components/Modals/Auth/CreateCommunity/CreateCommunityModal.tsx
+++ src/components/Modals/Auth/CreateCommunity/CreateCommunityModal.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import {
   Modal,
   ModalOverlay,
@@ -9,8 +11,16 @@ import {
   Button,
   Box,
   Divider,
+  Text,
+  Input,
+  Stack,
+  Checkbox,
+  Flex,
+  Icon,
 } from "@chakra-ui/react";
-import React from "react";
+
+import { BsFillEyeFill, BsFillPersonFill } from "react-icons/bs";
+import { HiLockClosed } from "react-icons/hi";
 
 type CreateCommunityModalProps = {
   open: boolean;
@@ -21,9 +31,24 @@ const CreateCommunityModal: React.FC<CreateCommunityModalProps> = ({
   open,
   handleClose,
 }) => {
+  const [communityName, setCommunityName] = useState("");
+  const [charsRemaining, setCharsRemaining] = useState(21);
+  const [communityType, setCommunityType] = useState("public");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value.length > 21) return;
+
+    setCommunityName(event.target.name);
+    // recalculating chars remaining for input
+    setCharsRemaining(21 - event.target.value.length);
+  };
+
+  const onCommunityTypeChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {};
   return (
     <>
-      <Modal isOpen={open} onClose={handleClose}>
+      <Modal isOpen={open} onClose={handleClose} size={"lg"}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader
@@ -37,12 +62,112 @@ const CreateCommunityModal: React.FC<CreateCommunityModalProps> = ({
           <Box pl={3} pr={3}>
             <Divider />
             <ModalCloseButton />
-            <ModalBody></ModalBody>
+            <ModalBody
+              display={"flex"}
+              flexDirection={"column"}
+              padding={"10px 0px"}
+            >
+              <Text fontSize={15} fontWeight={700}>
+                Name
+              </Text>
+              <Text fontSize={11} color={"gray.500"}>
+                Community names including capitalization cannot be changed
+              </Text>
+              <Text
+                position={"relative"}
+                top={"20px"}
+                left={"10px"}
+                width={"20px"}
+                color={"gray.400"}
+              >
+                r/
+              </Text>
+              <Input
+                position={"relative"}
+                top={""}
+                value={communityName}
+                size={"sm"}
+                pl="22px"
+                onChange={handleChange}
+              />
+              <Text
+                fontSize={"9pt"}
+                color={charsRemaining === 0 ? "red" : "gray.500"}
+              >
+                {charsRemaining} Characters remaining
+              </Text>
+              <Box my={4}>
+                <Text fontWeight={600} fontSize={15}>
+                  Community Type
+                </Text>
+                <Stack spacing={2}>
+                  <Checkbox
+                    name="public"
+                    isChecked={communityName === "public"}
+                    onChange={onCommunityTypeChange}
+                  >
+                    <Flex align={"center"}>
+                      <Icon as={BsFillPersonFill} color={"gray.500"} mr={2} />
+                      <Text fontSize={"10pt"} mr={1}>
+                        Public
+                      </Text>
+                      <Text fontSize={"8pt"} color={"gray.500"} pt={1}>
+                        Anyone can view, post, and comment to this community
+                      </Text>
+                    </Flex>
+                  </Checkbox>
+                  <Checkbox
+                    name="restricted"
+                    isChecked={communityName === "restricted"}
+                    onChange={onCommunityTypeChange}
+                  >
+                    <Flex align={"center"}>
+                      <Icon as={BsFillEyeFill} color={"gray.500"} mr={2} />
+                      <Text fontSize={"10pt"} mr={1}>
+                        Restricted
+                      </Text>
+                      <Text fontSize={"8pt"} color={"gray.500"} pt={1}>
+                        Anyone can view this community, but only approved users
+                        can post
+                      </Text>
+                    </Flex>
+                  </Checkbox>
+                  <Checkbox
+                    name="private"
+                    isChecked={communityName === "private"}
+                    onChange={onCommunityTypeChange}
+                  >
+                    <Flex align={"center"}>
+                      <Icon as={HiLockClosed} color={"gray.500"} mr={2} />
+                      <Text fontSize={"10pt"} mr={1}>
+                        Private
+                      </Text>
+                      <Text fontSize={"8pt"} color={"gray.500"} pt={1}>
+                        Only approved users can view and post to this community
+                      </Text>
+                    </Flex>
+                  </Checkbox>
+                </Stack>
+              </Box>
+            </ModalBody>
           </Box>
 
-          <ModalFooter>
-            <Button colorScheme="blue" onClick={handleClose}>
-              Close
+          <ModalFooter bg={"gray.100"} borderRadius={"0px 0px 10px 10px"}>
+            <Button
+              variant={"outline"}
+              height={"30px"}
+              colorScheme="blue"
+              onClick={handleClose}
+            >
+              Cancel
+            </Button>
+            <Button
+              variant={"solid"}
+              height={"30px"}
+              colorScheme="blue"
+              onClick={() => {}}
+            >
+              Create Community
             </Button>
           </ModalFooter>
         </ModalContent>
